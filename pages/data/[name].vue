a6e9280726e2094ca17dd0c84bbf075ccee474b8
Riaz Uddin Masum <masum@pop-os.localdomain>
2022-11-02 01:52:38 +0600

    page

--- pages/data/[name].vue
+++ pages/data/[name].vue
@@ -1,6 +1,6 @@
 <template>
   <div v-editable="blok" class="px-4">
-    <StoryblokComponent v-for="blok in blok.body" :key="blok._uid" :blok="blok" />
+    <StoryblokComponent v-for="blok in blok?.body" :key="blok._uid" :blok="blok" />
   </div>
 </template>
  
